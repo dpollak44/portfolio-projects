a8374a566a7fb5a4e75dbd9df316df4d549ee1fb
dpollak44 <49260784+dpollak44@users.noreply.github.com>
2022-12-07 17:46:31 -0500

    small comments and cleanup

--- Appointment-Reservations/server/modules/appointment.js
+++ Appointment-Reservations/server/modules/appointment.js
@@ -4,6 +4,8 @@ const asyncTimeout = (ms) => {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+// This function is called when a patient makes an appointment
+// It takes an appointment id and an amount of time to wait before cancelling the appointment if not confirmed
 const cancelAppointmentIfNotConfirmed = async(appointment_id,ms) => {
     await asyncTimeout(ms);
     const appointment = await PatientAppointment.findOne({
